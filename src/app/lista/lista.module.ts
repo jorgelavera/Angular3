e775f4458073bb0ee25e7a8d0e80b1acb57caf4a
Jorge Alejandro Lavera <jlavera@gmail.com>
2023-04-07 19:39:21 -0300

    empezando nuevo

---
+++ src/app/lista/lista.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaComponent } from './lista.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListaModule { }
---
+++ src/app/toolbar/toolbar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './toolbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    ToolbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToolbarModule { }
