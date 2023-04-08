6dda7ccc69a39920095f90b2e7a6ed95ee7de7c3
Jorge Alejandro Lavera <jlavera@gmail.com>
2023-04-07 21:24:19 -0300

    componentes funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ListaComponent } from './lista/lista.component';
+import { ItemListComponent } from './shared/components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    ListaComponent,
+    ItemListComponent
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista.component.css']
 })
 export class ListaComponent {
-
+  alumnos: Array<string> = [
+    'Jorge',
+    'Alejandro',
+    'Pedro',
+    'Mirta'
+  ];
 }
